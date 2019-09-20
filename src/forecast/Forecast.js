43dfc2fa3c47921088c206ecf993acef714c8fb4
LAPTOP-6GLD5642\João Heckmann <notbestkat@gmail.com>
2019-09-20 14:22:06 -0300

    Creating forecast and five day forecast

---
+++ src/forecast/Forecast.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+const Forecast = () => {
+    //TODO: stateful component with day and temperature data
+    return (
+        <div className="container"></div>
+    );
+};
+
+class FiveDayForecast extends Component {
+
+    render() {
+        return(<Forecast></Forecast>);
+    }
+}
+
+export default FiveDayForecast;
